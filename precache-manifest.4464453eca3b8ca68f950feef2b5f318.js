06f01a8908992d6eb86d614479fdf9d157310d18
gonzalopimentel <gonzalo.pimentel.ramirez@gmail.com>
2020-08-05 14:16:46 -0500

    Updates

---
+++ precache-manifest.4464453eca3b8ca68f950feef2b5f318.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a55274b67f13113e082c28078404a2",
+    "url": "/First-project-with-React/index.html"
+  },
+  {
+    "revision": "9612e3218c32e4f1a324",
+    "url": "/First-project-with-React/static/css/main.83e5659c.chunk.css"
+  },
+  {
+    "revision": "78f15cedf1a3d74478fa",
+    "url": "/First-project-with-React/static/js/2.bcbd28c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/First-project-with-React/static/js/2.bcbd28c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9612e3218c32e4f1a324",
+    "url": "/First-project-with-React/static/js/main.64a42e9b.chunk.js"
+  },
+  {
+    "revision": "5ed8bc5034e1101491da",
+    "url": "/First-project-with-React/static/js/runtime-main.c24b7268.js"
+  },
+  {
+    "revision": "e9b52209dbaac935c802f5ed9b4102ef",
+    "url": "/First-project-with-React/static/media/background.e9b52209.png"
+  },
+  {
+    "revision": "6d0f38aaa66022eef8477e62b86c8283",
+    "url": "/First-project-with-React/static/media/background2x.6d0f38aa.png"
+  },
+  {
+    "revision": "45598562077bdc84281b5330ff4d283e",
+    "url": "/First-project-with-React/static/media/corazon.45598562.png"
+  },
+  {
+    "revision": "ba2e415e817351fb402d07afd2be5ceb",
+    "url": "/First-project-with-React/static/media/easter-a.ba2e415e.png"
+  },
+  {
+    "revision": "7d7a81dcaa54ebefe86d47f90c6bb589",
+    "url": "/First-project-with-React/static/media/easter-b.7d7a81dc.png"
+  },
+  {
+    "revision": "0db2afe8201f90349fca3d76da2019ef",
+    "url": "/First-project-with-React/static/media/icomoon.0db2afe8.ttf"
+  },
+  {
+    "revision": "85c2eed24cd21f70d9880d00da02f6bd",
+    "url": "/First-project-with-React/static/media/icomoon.85c2eed2.eot"
+  },
+  {
+    "revision": "9f215c1e5e1c89f82c1aff021398d605",
+    "url": "/First-project-with-React/static/media/icomoon.9f215c1e.svg"
+  },
+  {
+    "revision": "e197b52e37ffa7f2e2e6e3d4562c9da4",
+    "url": "/First-project-with-React/static/media/icomoon.e197b52e.woff"
+  },
+  {
+    "revision": "25c6f861860963cb105716cc999b5174",
+    "url": "/First-project-with-React/static/media/invie-acustica.25c6f861.png"
+  },
+  {
+    "revision": "e039fff97126b1b18d68f4be45bf6d25",
+    "url": "/First-project-with-React/static/media/invie-classic.e039fff9.png"
+  },
+  {
+    "revision": "70297162b2ac4eb6ac54a0ddd5f85bfe",
+    "url": "/First-project-with-React/static/media/team-platzi-azul.70297162.jpg"
+  }
+]);
